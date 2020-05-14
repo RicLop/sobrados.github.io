2b56e309c1f4c8032bfc368c470b8def22009f5b
mogba <mogba66@com.blankspace>
2020-05-14 01:42:27 -0300

    Adição de envio de e-mail

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ $(document).ready(function() {
     var screenWidth = $(window).width();
     $('navbar-header').css('width', screenWidth + 'px');
  });
+
+function sendEmail() {
+    let name = document.getElementById('contactName').value;
+    let whatsApp = document.getElementById('contactWhatsApp').value;
+    let email = document.getElementById('contactEmail').value;
+    let preferredTime = document.getElementById('contactPreferredTime').value;
+    // let grossIncome = document.getElementById('contactGrossIncome').value;
+    let message = `Olá! Meu nome é ${name}. Gostaria de maiores informações sobre os sobrados da ROS Imóveis! Favor retornar por volta das ${preferredTime}, pelo WhatsApp, no número ${whatsApp}, ou pelo e-mail ${email}.`;
+    window.open('mailto:gabrieldemorais6@gmail.com?body=' + message + '&subject=Sobrados ROS Imóveis', 'mail');
+    event.preventDefault();
+}
