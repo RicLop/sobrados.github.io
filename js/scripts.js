9eb0ba0fd865d13a6cd333fd3afbefba1439c657
mogba <mogba66@com.blankspace>
2020-05-14 08:36:50 -0300

    Alterações no envio de e-mail

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ function sendEmail() {
     let whatsApp = document.getElementById('contactWhatsApp').value;
     let email = document.getElementById('contactEmail').value;
     let preferredTime = document.getElementById('contactPreferredTime').value;
-    // let grossIncome = document.getElementById('contactGrossIncome').value;
-    let message = `Olá! Meu nome é ${name}. Gostaria de maiores informações sobre os sobrados da ROS Imóveis! Favor retornar por volta das ${preferredTime}, pelo WhatsApp, no número ${whatsApp}, ou pelo e-mail ${email}.`;
-    window.open('mailto:gabrieldemorais6@gmail.com?body=' + message + '&subject=Sobrados ROS Imóveis', 'mail');
+    let grossIncome = document.getElementById('contactGrossIncome').value;
+    let message = `Olá! Meu nome é ${name}. Gostaria de maiores informações sobre os sobrados da ROS Imóveis! Possuo renda bruta de ${grossIncome} e o melhor horário para contato é ${preferredTime}. Favor retornar pelo WhatsApp, no número ${whatsApp}, ou pelo e-mail ${email}.`;
+    window.open('mailto:10semnome01@gmail.com?body=' + message + '&subject=Sobrados ROS Imóveis', 'mail');
     event.preventDefault();
 }
